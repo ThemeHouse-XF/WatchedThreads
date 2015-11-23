6888c2eba6bcf8d8e9840833b57d5158eb65175a
Jake B <jake.booher@gmail.com>
2015-11-23 10:56:12 -0600

    Initial Commit

---
+++ library/ThemeHouse/WatchedThreads/Install/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ThemeHouse_WatchedThreads_Install_Controller extends ThemeHouse_Install
+{
+
+    protected $_resourceManagerUrl = 'http://xenforo.com/community/resources/watched-threads-by-th.3835/';
+
+    protected $_minVersionId = 1020000;
+
+    protected $_minVersionString = '1.2.0';
+}
---
+++ library/ThemeHouse/WatchedThreads/Listener/FileHealthCheck.php
@@ -0,0 +1,21 @@
+<?php
+
+class ThemeHouse_WatchedThreads_Listener_FileHealthCheck
+{
+
+    public static function fileHealthCheck(XenForo_ControllerAdmin_Abstract $controller, array &$hashes)
+    {
+        $hashes = array_merge($hashes,
+            array(
+                'library/ThemeHouse/WatchedThreads/Install/Controller.php' => '280ed54999178dd2a5b77948a20d6506',
+                'library/ThemeHouse/Install.php' => '18f1441e00e3742460174ab197bec0b7',
+                'library/ThemeHouse/Install/20151109.php' => '2e3f16d685652ea2fa82ba11b69204f4',
+                'library/ThemeHouse/Deferred.php' => 'ebab3e432fe2f42520de0e36f7f45d88',
+                'library/ThemeHouse/Deferred/20150106.php' => 'a311d9aa6f9a0412eeba878417ba7ede',
+                'library/ThemeHouse/Listener/ControllerPreDispatch.php' => 'fdebb2d5347398d3974a6f27eb11a3cd',
+                'library/ThemeHouse/Listener/ControllerPreDispatch/20150911.php' => 'f2aadc0bd188ad127e363f417b4d23a9',
+                'library/ThemeHouse/Listener/InitDependencies.php' => '8f59aaa8ffe56231c4aa47cf2c65f2b0',
+                'library/ThemeHouse/Listener/InitDependencies/20150212.php' => 'f04c9dc8fa289895c06c1bcba5d27293',
+            ));
+    }
+}
